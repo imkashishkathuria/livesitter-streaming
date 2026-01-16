b2ecaf293d27ce3b0146a984ce9fb7af3bcf6e00
kashish <kashish@growthzi.in>
2026-01-16 13:03:04 +0530

    cameras page

--- client/src/components/NewButton.jsx
+++ client/src/components/NewButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewButton = ({text}) => {
   return (
-    <div className='bg-gray-300 px-4 w-[80%] h-25 flex justify-center items-center m-auto'>
+    <div className='bg-gray-200 px-4 w-[80%] h-25 flex justify-center items-center m-auto'>
       <button className='text-white text-md px-6 py-1 flex justify-center items-center gap-2 bg-green-700 rounded-full hover:bg-green-800 cursor-pointer'>
         <p className='text-2xl font-extrabold mb-1'>+</p>
         {text}
--- client/src/pages/Cameras.jsx
+++ client/src/pages/Cameras.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import PageHeader from '../components/PageHeader'
+import NewButton from '../components/NewButton'
+import Footer from '../components/Footer'
 
 const Cameras = () => {
   return (
     <div>
       <Navbar />
       <PageHeader text="CAMERAS" />
+
+
+      <div className="px-6 py-10">
+        {/* Table Header */}
+        <div className="grid grid-cols-5 px-6 py-4 text-lg text-gray-400 font-medium">
+          <div>Alias</div>
+          <div>Name</div>
+          <div>Features</div>
+          <div>Enabled</div>
+          <div>Online</div>
+        </div>
+
+        {/* Divider */}
+        <div className="border-t border-gray-300" />
+
+        {/* Empty State */}
+        <div className="px-6 py-6 text-lg text-gray-400 font-medium">
+          No data to display
+        </div>
+      </div>
+      <div className='py-10'><NewButton text="NEW CAMERA" /></div>
+      
+      <div className='pt-43'>
+        <Footer />
+      </div>
     </div>
   )
 }
